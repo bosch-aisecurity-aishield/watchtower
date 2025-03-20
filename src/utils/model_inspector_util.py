631f7220b4f27a8d927ffc8f8057b8d33a28c614
Maurya Pankaj Kanta (BGSW/PAS-PRM) <PankajKanta.Maurya@in.bosch.com>
2025-03-20 15:44:24 +0530

    updating pickle scanner version and removing tranformer library and it's related code as not being used.

--- src/utils/model_inspector_util.py
+++ src/utils/model_inspector_util.py
@@ -67,185 +67,165 @@ from safetensors import safe_open
 # load external files for safe model load (.h5, .pb, .keras)
 from external_files import model_safe_load
 
-from transformers import (
-    AutoModel,
-    AutoModelForSequenceClassification,
-    AutoModelForTokenClassification,
-    AutoModelForQuestionAnswering,
-    AutoModelForCausalLM,
-    AutoModelForMaskedLM,
-    AutoModelForSeq2SeqLM,
-    AutoModelForMultipleChoice,
-    AutoModelForImageClassification,
-    AutoModelForImageSegmentation,
-    AutoModelForVision2Seq,
-    AutoModelForAudioClassification,
-    AutoModelForDocumentQuestionAnswering
-)
-
-
 # list of keras official layers (From Tensorflow 2.16.1)
 keras_layers_tf_2_16_1 = ['Activation',
- 'ActivityRegularization',
- 'Add',
- 'AdditiveAttention',
- 'AlphaDropout',
- 'Attention',
- 'AutoContrast',
- 'Average',
- 'AveragePooling1D',
- 'AveragePooling2D',
- 'AveragePooling3D',
- 'AvgPool1D',
- 'AvgPool2D',
- 'AvgPool3D',
- 'BatchNormalization',
- 'Bidirectional',
- 'CategoryEncoding',
- 'CenterCrop',
- 'Concatenate',
- 'Conv1D',
- 'Conv1DTranspose',
- 'Conv2D',
- 'Conv2DTranspose',
- 'Conv3D',
- 'Conv3DTranspose',
- 'ConvLSTM1D',
- 'ConvLSTM2D',
- 'ConvLSTM3D',
- 'Convolution1D',
- 'Convolution1DTranspose',
- 'Convolution2D',
- 'Convolution2DTranspose',
- 'Convolution3D',
- 'Convolution3DTranspose',
- 'Cropping1D',
- 'Cropping2D',
- 'Cropping3D',
- 'Dense',
- 'DepthwiseConv1D',
- 'DepthwiseConv2D',
- 'Discretization',
- 'Dot',
- 'Dropout',
- 'ELU',
- 'EinsumDense',
- 'Embedding',
- 'Flatten',
- 'FlaxLayer',
- 'GRU',
- 'GRUCell',
- 'GaussianDropout',
- 'GaussianNoise',
- 'GlobalAveragePooling1D',
- 'GlobalAveragePooling2D',
- 'GlobalAveragePooling3D',
- 'GlobalAvgPool1D',
- 'GlobalAvgPool2D',
- 'GlobalAvgPool3D',
- 'GlobalMaxPool1D',
- 'GlobalMaxPool2D',
- 'GlobalMaxPool3D',
- 'GlobalMaxPooling1D',
- 'GlobalMaxPooling2D',
- 'GlobalMaxPooling3D',
- 'GroupNormalization',
- 'GroupQueryAttention',
- 'HashedCrossing',
- 'Hashing',
- 'Identity',
- 'Input',
- 'InputLayer',
- 'InputSpec',
- 'IntegerLookup',
- 'JaxLayer',
- 'LSTM',
- 'LSTMCell',
- 'Lambda',
- 'Layer',
- 'LayerNormalization',
- 'LeakyReLU',
- 'Masking',
- 'MaxPool1D',
- 'MaxPool2D',
- 'MaxPool3D',
- 'MaxPooling1D',
- 'MaxPooling2D',
- 'MaxPooling3D',
- 'Maximum',
- 'MelSpectrogram',
- 'Minimum',
- 'MultiHeadAttention',
- 'Multiply',
- 'Normalization',
- 'PReLU',
- 'Permute',
- 'Pipeline',
- 'RNN',
- 'RandomBrightness',
- 'RandomContrast',
- 'RandomCrop',
- 'RandomFlip',
- 'RandomHeight',
- 'RandomRotation',
- 'RandomTranslation',
- 'RandomWidth',
- 'RandomZoom',
- 'ReLU',
- 'RepeatVector',
- 'Rescaling',
- 'Reshape',
- 'Resizing',
- 'SeparableConv1D',
- 'SeparableConv2D',
- 'SeparableConvolution1D',
- 'SeparableConvolution2D',
- 'SimpleRNN',
- 'SimpleRNNCell',
- 'Softmax',
- 'Solarization',
- 'SpatialDropout1D',
- 'SpatialDropout2D',
- 'SpatialDropout3D',
- 'SpectralNormalization',
- 'StackedRNNCells',
- 'StringLookup',
- 'Subtract',
- 'TFSMLayer',
- 'TextVectorization',
- 'ThresholdedReLU',
- 'TimeDistributed',
- 'TorchModuleWrapper',
- 'UnitNormalization',
- 'UpSampling1D',
- 'UpSampling2D',
- 'UpSampling3D',
- 'Wrapper',
- 'ZeroPadding1D',
- 'ZeroPadding2D',
- 'ZeroPadding3D',
- '__builtins__',
- '__cached__',
- '__doc__',
- '__file__',
- '__loader__',
- '__name__',
- '__package__',
- '__path__',
- '__spec__',
- 'add',
- 'average',
- 'concatenate',
- 'deserialize',
- 'dot',
- 'maximum',
- 'minimum',
- 'multiply',
- 'serialize',
- 'subtract']
-
-
-
+                          'ActivityRegularization',
+                          'Add',
+                          'AdditiveAttention',
+                          'AlphaDropout',
+                          'Attention',
+                          'AutoContrast',
+                          'Average',
+                          'AveragePooling1D',
+                          'AveragePooling2D',
+                          'AveragePooling3D',
+                          'AvgPool1D',
+                          'AvgPool2D',
+                          'AvgPool3D',
+                          'BatchNormalization',
+                          'Bidirectional',
+                          'CategoryEncoding',
+                          'CenterCrop',
+                          'Concatenate',
+                          'Conv1D',
+                          'Conv1DTranspose',
+                          'Conv2D',
+                          'Conv2DTranspose',
+                          'Conv3D',
+                          'Conv3DTranspose',
+                          'ConvLSTM1D',
+                          'ConvLSTM2D',
+                          'ConvLSTM3D',
+                          'Convolution1D',
+                          'Convolution1DTranspose',
+                          'Convolution2D',
+                          'Convolution2DTranspose',
+                          'Convolution3D',
+                          'Convolution3DTranspose',
+                          'Cropping1D',
+                          'Cropping2D',
+                          'Cropping3D',
+                          'Dense',
+                          'DepthwiseConv1D',
+                          'DepthwiseConv2D',
+                          'Discretization',
+                          'Dot',
+                          'Dropout',
+                          'ELU',
+                          'EinsumDense',
+                          'Embedding',
+                          'Flatten',
+                          'FlaxLayer',
+                          'GRU',
+                          'GRUCell',
+                          'GaussianDropout',
+                          'GaussianNoise',
+                          'GlobalAveragePooling1D',
+                          'GlobalAveragePooling2D',
+                          'GlobalAveragePooling3D',
+                          'GlobalAvgPool1D',
+                          'GlobalAvgPool2D',
+                          'GlobalAvgPool3D',
+                          'GlobalMaxPool1D',
+                          'GlobalMaxPool2D',
+                          'GlobalMaxPool3D',
+                          'GlobalMaxPooling1D',
+                          'GlobalMaxPooling2D',
+                          'GlobalMaxPooling3D',
+                          'GroupNormalization',
+                          'GroupQueryAttention',
+                          'HashedCrossing',
+                          'Hashing',
+                          'Identity',
+                          'Input',
+                          'InputLayer',
+                          'InputSpec',
+                          'IntegerLookup',
+                          'JaxLayer',
+                          'LSTM',
+                          'LSTMCell',
+                          'Lambda',
+                          'Layer',
+                          'LayerNormalization',
+                          'LeakyReLU',
+                          'Masking',
+                          'MaxPool1D',
+                          'MaxPool2D',
+                          'MaxPool3D',
+                          'MaxPooling1D',
+                          'MaxPooling2D',
+                          'MaxPooling3D',
+                          'Maximum',
+                          'MelSpectrogram',
+                          'Minimum',
+                          'MultiHeadAttention',
+                          'Multiply',
+                          'Normalization',
+                          'PReLU',
+                          'Permute',
+                          'Pipeline',
+                          'RNN',
+                          'RandomBrightness',
+                          'RandomContrast',
+                          'RandomCrop',
+                          'RandomFlip',
+                          'RandomHeight',
+                          'RandomRotation',
+                          'RandomTranslation',
+                          'RandomWidth',
+                          'RandomZoom',
+                          'ReLU',
+                          'RepeatVector',
+                          'Rescaling',
+                          'Reshape',
+                          'Resizing',
+                          'SeparableConv1D',
+                          'SeparableConv2D',
+                          'SeparableConvolution1D',
+                          'SeparableConvolution2D',
+                          'SimpleRNN',
+                          'SimpleRNNCell',
+                          'Softmax',
+                          'Solarization',
+                          'SpatialDropout1D',
+                          'SpatialDropout2D',
+                          'SpatialDropout3D',
+                          'SpectralNormalization',
+                          'StackedRNNCells',
+                          'StringLookup',
+                          'Subtract',
+                          'TFSMLayer',
+                          'TextVectorization',
+                          'ThresholdedReLU',
+                          'TimeDistributed',
+                          'TorchModuleWrapper',
+                          'UnitNormalization',
+                          'UpSampling1D',
+                          'UpSampling2D',
+                          'UpSampling3D',
+                          'Wrapper',
+                          'ZeroPadding1D',
+                          'ZeroPadding2D',
+                          'ZeroPadding3D',
+                          '__builtins__',
+                          '__cached__',
+                          '__doc__',
+                          '__file__',
+                          '__loader__',
+                          '__name__',
+                          '__package__',
+                          '__path__',
+                          '__spec__',
+                          'add',
+                          'average',
+                          'concatenate',
+                          'deserialize',
+                          'dot',
+                          'maximum',
+                          'minimum',
+                          'multiply',
+                          'serialize',
+                          'subtract']
 
 
 def contains_pickled_data_direct_scan(h5_file_path: str):
@@ -349,28 +329,28 @@ def unsafe_check_h5(model_path: str):
     lambda_suspicious_detected = False
     moderate_lamda_suspicious_detected = False
     unsafe_layer_detected = False
- 
+
     try:
-       
+
         # Check for suspicious patterns in Lambda layers
         suspicious_patterns = [
-            "execute_command", "os.system","ossystem","system", "subprocess.run", "subprocess.Popen",
+            "execute_command", "os.system", "ossystem", "system", "subprocess.run", "subprocess.Popen",
             "open(", "eval(", "exec(", "import os", "import subprocess", "pickle.load", "pickle.loads", "joblib.load"
         ]
         moderate_suspicious_patterns = ["numpy.load"]
-    
-        # Replaced with safe load functions
-        h5_layers, unsafe_h5_layers  = model_safe_load.safe_load_model_h5_format(model_path)
 
-        if (unsafe_h5_layers): #list is not empty
+        # Replaced with safe load functions
+        h5_layers, unsafe_h5_layers = model_safe_load.safe_load_model_h5_format(model_path)
+
+        if (unsafe_h5_layers):  #list is not empty
             unsafe_layer_detected = True
-        
+
         # Check for pickled data in the .h5 file
         magic_number_detected_status = contains_pickled_data_direct_scan(model_path)
- 
+
         # non_keras_layer_detected = any(not hasattr(keras.layers, type(layer).__name__) for layer in model.layers)
         non_keras_layer_detected = not any(layer in keras_layers_tf_2_16_1 for layer in h5_layers)
- 
+
         # If magic numbers are detected OR a suspicious Lambda layer is detected OR a non-Keras layer is detected,
         # flag the file
         if magic_number_detected_status:
@@ -379,7 +359,7 @@ def unsafe_check_h5(model_path: str):
 
         elif unsafe_layer_detected:
             tool_output.append("lambda_suspicious#Severity:High - Potential security concern detected "
-                                "in the model, unsafe layers such as Lambda or Custom can execute arbitrary code")
+                               "in the model, unsafe layers such as Lambda or Custom can execute arbitrary code")
             print(
                 "\nPotential security concern detected in the model.Lambda layers can execute "
                 "arbitrary code.")
@@ -391,7 +371,7 @@ def unsafe_check_h5(model_path: str):
             print(
                 "\nPotential security concern detected in the model, Non-Keras custom layers can contain arbitrary "
                 "code but require explicit loading.")
- 
+
     except ValueError as e:
         if "Unknown layer" in str(e):
             tool_output.append("Unknown layer#Severity:Medium - Potential security concern detected "
@@ -400,11 +380,11 @@ def unsafe_check_h5(model_path: str):
             print(
                 "Model contains an unknown custom layer. This can be a potential security risk"
                 "Requires explicit loading.")
- 
+
     except Exception as e:
         tool_output.append("Error loading model#Severity:Low - Unable to load the Model {}".format(str(e)))
         print("Error loading model: {}".format(str(e)))
- 
+
     return tool_output
 
 
@@ -442,7 +422,8 @@ def unsafe_check_pb(model_path: str):
         tool_output.append("Error loading model#Severity:Low - Unable to load the Model {}".format(str(e)))
 
     return tool_output
-    
+
+
 # SafeTensor Check
 def unsafe_check_safetensors(model_path: str):
     """
@@ -472,27 +453,28 @@ def unsafe_check_safetensors(model_path: str):
             If no vulnerabilities are found, an empty list will be returned.
 
         """
-    
+
     tool_output = list()
-    model_layers = list() # Create a list to append all the layers of the safetensors model
-    
+    model_layers = list()  # Create a list to append all the layers of the safetensors model
+
     # List of suspicious patterns to detect
     suspicious_patterns = [
         "execute_command", "os.system", "subprocess.run", "subprocess.Popen",
         "open", "eval", "exec", "import os", "import subprocess", "pickle.load", "pickle.loads", "joblib.load"
     ]
     moderate_suspicious_patterns = ["numpy.load"]
-    
+
     try:
         # Load the model from the SafeTensors file
-        with safe_open(model_path, framework="pt") as f: # Since the base model is a pytorch model, specifying the framework as pt
+        with safe_open(model_path,
+                       framework="pt") as f:  # Since the base model is a pytorch model, specifying the framework as pt
             layer_names = f.keys()
             model_layers.extend(layer_names)
     except Exception as e:
         print(f"Error loading model: {e}")
         tool_output.append("Error loading model#Severity:Low - Unable to load the Model {}".format(str(e)))
         return tool_output
-    
+
     # List of standard Pytorch base safetensor layers
     standard_layers = [
         'linear', 'conv2d', 'batchnorm2d', 'relu', 'maxpool2d', 'avgpool2d',
@@ -516,20 +498,20 @@ def unsafe_check_safetensors(model_path: str):
             layer_name = layer.split('.')[-2]  # Get the actual layer name
 
             if any(layer_name == suspicious_layer for suspicious_layer in suspicious_layers):
-                lamda_suspicious_layer=True
+                lamda_suspicious_layer = True
                 break
             elif any(layer_name not in standard_layers for layer_name in layer.split('.')):
                 non_standard_layer = True
-                
+
         for pattern in suspicious_patterns:
             if any(pattern in layer for layer in model_layers):
-                high_suspicious_pattern=True
+                high_suspicious_pattern = True
                 break
 
         for pattern in moderate_suspicious_patterns:
             if any(pattern in layer for layer in model_layers):
-                moderate_suspicious_pattern=True
-        
+                moderate_suspicious_pattern = True
+
         if lamda_suspicious_layer or high_suspicious_pattern:
             tool_output.append("Lamda_or_Embedding_layer#Severity:High - Detected Lambda operations in the "
                                ".safetensors model or Detected suspicious pattern  in the .safetensors model. "
@@ -547,13 +529,13 @@ def unsafe_check_safetensors(model_path: str):
                                "can introduce vulnerabilities. However, these custom operations typically "
                                " require explicit loading, which means there's an additional step before potential "
                                "execution. or any suspicious pattern may indicate to attempt to load the arbitrary data when the model is loaded")
-                
+
     except Exception as e:
         print("Failed to Perform unsafe-check-safetensors due to: {}".format(str(e)))
-        
+
     return tool_output
-    
-    
+
+
 def scan_pickle_file(path: str):
     """
     This function takes pickle file path as input to scan for static vulnerability in pickle file
